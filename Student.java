66c4dbc40ba631ff20645ed68b66c5c539064eca
mattetress <mattetress@me.com>
2019-05-20 17:18:53 -0500

    create student and set id

--- Student.java
+++ Student.java
@@ -1,5 +1,57 @@
 package studentDatabaseApp;
 
+import java.util.Scanner;
+
 public class Student {
+	
+	private String firstName;
+	private String lastName;
+	private int gradeYear;
+	private String studentID;
+	private String courses;
+	private int tuitionBalance;
+	private static int costOfCourse = 600;
+	private static int id = 1000;
+	
+	
+	// Constructor: prompt to enter name and year
+	public Student() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter student first name: ");
+		this.firstName = in.nextLine();
+	
+		System.out.print("Enter student last name: ");
+		this.lastName = in.nextLine();
+		
+		System.out.print("1 - Freshman"
+				+ "\n2 - Sophomore"
+				+ "\n3 - Junior"
+				+ "\n4 - Senior"
+				+ "\nEnter student class level: ");
+		this.gradeYear = in.nextInt();
+		
+		setStudentID();
+		
+		System.out.println(studentID + " - " + firstName + " " + lastName + " - " + gradeYear);
+		
+		
+	}
+	
+	// Generate ID 
+	private void setStudentID() {
+		// Grade level + id 
+		id++;
+		this.studentID = gradeYear + "" + id;
+	}
+	
+	
+	
+	// enroll in courses
+	
+	// view balance 
+	
+	// pay tuition
+	
+	// show status
 
 }
--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -3,7 +3,12 @@ package studentDatabaseApp;
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Student stu1 = new Student();
+		
+		// Ask how many users we want to add
+		
+		// Create n number of new Students
 
 	}
 
